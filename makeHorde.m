dc93bf3a2bcbf290da052c044ea4fd97573bef5e
Mario Kleiner <mario.kleiner.de@gmail.com>
2025-10-06 05:00:14 +0100

    makeHorde/OSX: Update for recent PTB and macOS. Some Apple Silicon prep.
    Note that for some fracking reason we now need to use install_name_tool to change
    the absolute path of the Horde3D framework libraries to the mex files @loader_path.
    
    Why? No clue, just a new thing we need, now that we are building under macOS 13,
    or with XCode 15.2. Another 3 hours of my life I'll never get back...

--- makeHorde.m
+++ makeHorde.m
@@ -4,7 +4,7 @@ function makeHorde
 % Requirements: Needs all relevant Horde3D SDK header files and libraries
 % inside the local subfolder HordeEngineSDK/
 %
-% Needs a recent version of Psychtoolbox-3 (V3.0.15 or later) installed.
+% Needs a recent version of Psychtoolbox-3 (V3.0.22 or later) installed.
 %
 % May need additional Horder runtime libraries installed on your system.
 %
@@ -22,6 +22,7 @@ function makeHorde
 % Updated 1.4.2012 in preparation of public release.
 % Updated 19.9.2015 for current Horde 1.0.0-Beta 5 git master and VR HMD
 % rendering support, as well as for OSX 10.10 build environment.
+% Updated 6.10.2025 For Octave 10 and later on macOS. Some prep for Apple Silicon.
 
 if IsWin
     if ~IsWin(1)
@@ -49,16 +50,19 @@ end
 if IsOSX
     % OS/X build:
     if IsOctave
-        mex -v Horde3DCore.cpp -I./HordeEngineSDK "-Wno-deprecated-declarations -mmacosx-version-min='10.11'" "-Wl,-headerpad_max_install_names,-F/Library/Frameworks/,-framework,Horde3D,-framework,Horde3DUtils"
+        mex -v Horde3DCore.cpp -I./HordeEngineSDK "-Wno-deprecated-declarations -mmacosx-version-min='10.13'" "-Wl,-headerpad_max_install_names,-F/Library/Frameworks/,-framework,Horde3D,-framework,Horde3DUtils"
         osxsetoctaverpath('Horde3DCore', './');
     else
-        mex -v Horde3DCore.cpp -I./HordeEngineSDK LDFLAGS="\$LDFLAGS -framework Horde3D -framework Horde3DUtils"
+        mex -v Horde3DCore.cpp -I./HordeEngineSDK LDFLAGS="\$LDFLAGS -F/Library/Frameworks -framework Horde3D -framework Horde3DUtils"
     end
 
-    if Is64Bit
-        movefile(['Horde3DCore.' mexext], ['HordeOSX64/Horde3DCore.' mexext]);
+    system(['install_name_tool -change /Users/kleinerm/projects/Horde3D/Horde3D/Source/Horde3DEngine/Horde3D.framework/Versions/A/Horde3D @loader_path/Horde3D.framework/Versions/A/Horde3D Horde3DCore.' mexext]);
+    system(['install_name_tool -change /Users/kleinerm/projects/Horde3D/Horde3D/Source/Horde3DUtils/Horde3DUtils.framework/Versions/A/Horde3DUtils @loader_path/Horde3DUtils.framework/Versions/A/Horde3DUtils Horde3DCore.' mexext]);
+
+    if IsARM
+        movefile(['Horde3DCore.' mexext], ['HordeOSXARM64/Horde3DCore.' mexext]);
     else
-        movefile(['Horde3DCore.' mexext], ['HordeOSX32/Horde3DCore.' mexext]);
+        movefile(['Horde3DCore.' mexext], ['HordeOSX64/Horde3DCore.' mexext]);
     end
 end
 
